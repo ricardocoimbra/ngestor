331b11b1826b3a195cab2b6c4ad4afa7d1799b57
Ricardo Coimbra <coimbra.rico@gmail.com>
2023-01-25 16:13:35 -0400

    initial commit

---
+++ database/migrations/2023_01_25_183850_create_clientes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // Nome, e-mail, rua, número, complemento, bairro, cidade, estado
+            $table->string('nome', 100);
+            $table->string('email');
+            $table->string('cep', 8);
+            $table->string('logradouro', 100);
+            $table->string('numero', 5);
+            $table->string('complemento', 150);
+            $table->string('bairro', 100);
+            $table->string('cidade', 100);
+            $table->string('estado', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
